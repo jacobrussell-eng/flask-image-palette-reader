7b310361eecc0422cd677478935771208f6a7f6a
Jacob Russell <8bitjacob@gmail.com>
2025-05-23 00:25:35 +0100

    Image Processing and Template Rendering
    The bulk of the work:
    - Wrote the top_ten_colours() function to open, process, and scan an image for individual rgb values, then count their occurances and return the top ten
    - Fed the results of this function to the request response to be rendered
    - Iterated over the results in the template to produce ten coloured divs with text to represent the rgb/hex value
    - Implemented logic to determine whether text colour should be black or white depending on the colour's luminance (Hex version needs patching)
    - Wrote the ReadMe and included links, screenshots and images
    
    Yet to go:
    - I want to be able to display the uploaded image next to the palette
    - Add dynamic text colour to Hex mode
    - Play around with the display more to create a nice interface which is also consistent with variable windowing

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# flask image palette reader
-A Flask app that scans an uploaded image and returns a palette of the most prevalent colours
+# flask image palette reader (WIP)
+A Flask app that scans an uploaded image and returns a palette of the most prevalent colours.
+<img src="static/screenshot.png" alt="screenshot" width="100%" height="auto">
+
+## Local Installation: 
+
+
+Using the following Bash commands, you can install the app and it run it at http://localhost:8000 (<i>requirements.txt is yet to be made as this project is incomplete</i>)
+
+```shell
+# 1. clone the repo:
+$ git clone https://github.com/jacobrussell-eng/flask-image-palette-reader.git
+$ cd flask-image-palette-reader
+
+# 2. install Python packages:
+$ pip install -r requirements.txt
+
+# 3. run:
+$ python app.py
+```
+
+## Resources:
+- Numpy
+- Pillow (PIL)
+- Relative Luminance Formula - [Wikipedia](https://en.wikipedia.org/wiki/Relative_luminance)\
+<img src="static/rgb_to_luminance_eq.jpg" alt="screenshot" width="40%" height="auto">
+- Background Colour - [Random Hex Generator](https://jacobrussell-eng.github.io/Random-Hex-Generator/)
--- app.py
+++ app.py
@@ -1,12 +1,58 @@
 from flask import Flask, render_template, request
+from PIL import Image, ImageOps
+from numpy import array, unique, argsort
 
 app = Flask(__name__)
 
+def top_ten_colours(file, code):
+    img = Image.open(file).convert('RGB')
+    img = ImageOps.posterize(img, 4)
+    img_array = array(img)
+
+    # Pure python method (slower):
+    # unique_colours = {}
+    # for column in img_array:
+    #     for colour in column:
+    #         tuple_colour = tuple(colour)
+    #         if tuple_colour in unique_colours:
+    #             unique_colours[tuple_colour] += 1
+    #         else:
+    #             unique_colours[tuple_colour] = 1
+    # unique_colours = sorted(unique_colours, key=lambda x: x[1], reverse=True)
+
+    # Flatten to no. Pixels and RGB only:
+    flat_array = img_array.reshape(-1, 3)
+    # Find Unique rows and their counts:
+    unique_colours, counts = unique(flat_array, axis=0, return_counts=True)
+
+    # Using argsort() returns the indices in order, which can be iterated through the other array:
+    if len(counts) <= 10:
+        top_ten_id = argsort(counts)[::-1]
+    else:
+        top_ten_id = (-counts).argsort()[:10]
+    top_ten_rgb = unique_colours[top_ten_id]
+    top_ten_rgb = [tuple(colour) for colour in top_ten_rgb]
+    print(top_ten_rgb)
+
+    # If hex values are requested:
+    if code == 'hex':
+        top_ten_hex = []
+        for key in top_ten_rgb:
+            top_ten_hex.append('#%02x%02x%02x' % key)
+        return top_ten_hex
+    else:
+        return top_ten_rgb
+
+# Process form input and Render template:
 @app.route('/', methods=['GET','POST'])
 def index():
+    colours = []
+    colour_code = 'rgb'
     if request.method == 'POST':
         f = request.files['file']
-    return render_template('index.html')
+        colour_code = request.form['colour_format']
+        colours = top_ten_colours(f.stream, colour_code)
+    return render_template('index.html', colours=colours, colour_code=colour_code)
 
 # Run locally:
 if __name__ == "__main__":
